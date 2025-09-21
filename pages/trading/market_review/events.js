a3377066c06b314841dd59ab6389d014df22b0a6
cleli683 <yifengli683@163.com>
2025-09-21 22:38:10 +0800

    新增文章

--- pages/trading/market_review/events.js
+++ pages/trading/market_review/events.js
@@ -1,17 +1,7 @@
 const events = [
     {
-        date: '2025-09-19',
-        title: '日度复盘',
-        url: '../market_review/articles/模版修改-1/index.html' // 示例链接，请根据您的文件结构修改
-    },
-    {
-        date: '2025-09-19',
-        title: '10月展望',
-        url: '../market_review/articles/模版修改-2/index.html' // 示例链接，请根据您的文件结构修改
-    },
-    {
-        date: '2025-09-30',
-        title: '日度复盘',
-        url: '../market_review/articles/模版修改-3/index.html' // 示例链接，请根据您的文件结构修改
+        date: '2025-09-21',
+        title: '周度复盘',
+        url: '../market_review/articles/债市周度复盘与学习笔记-20250921/index.html' // 示例链接，请根据您的文件结构修改
     }
 ];
